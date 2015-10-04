ce4a9dd0e75e1e9075c2e52b4618ab8f8dd2a156
MERIEM <mariam.chergui@devinci.fr>
2015-10-04 19:51:22 +0200

    Classe Item

---
+++ src/main/java/com/pbz4esilv/gildedrose/Inventory.java
@@ -0,0 +1,88 @@
+package com.pbz4esilv.gildedrose;
+import java.util.stream.Stream;
+
+public class Inventory {
+
+    private Item[] items;
+
+    public Inventory(Item[] items) {
+        super();
+        this.items = items;
+    }
+
+    public Inventory() {
+        super();
+        items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Conjured Mana Cake", 3, 6)
+        };
+
+
+
+    }
+
+     public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+
+    //Reusable method for updating Backstage and Aged Brie as nearly same constraints apply
+    public void updateBSAB(Item item) {
+        int itemSellin = item.getSellIn();
+        int itemQuality = item.getQuality();
+            if (itemSellin <= 10 && itemSellin > 5)
+                item.setQuality(itemQuality + 2 <= 50 ? itemQuality + 2 : 50);
+            else if (itemSellin <= 5)
+                item.setQuality(itemQuality + 3 <= 50 ? itemQuality + 3 : 50);
+    }
+
+    //Reusable method for updating conjured and other items
+    public void updateConjDefault(Item item, int beforeSellin){
+        int itemQualityConjured = item.getQuality();
+        item.setQuality(item.getSellIn() <= 0 ? (itemQualityConjured - beforeSellin*2 >= 0 ? itemQualityConjured - beforeSellin*2 : 0) :
+                        (itemQualityConjured - beforeSellin >= 0 ? itemQualityConjured - beforeSellin : 0)
+        );
+    }
+
+    public void updateQuality() {
+
+        Stream<Item> itemStream = Stream.of(items).filter(item -> !item.getName().equals("Sulfuras"));
+        itemStream.forEach(item -> item.setSellIn(item.getSellIn()-1));
+
+             for (Item item : items) {
+            switch (item.getName()) {
+
+                case "Backstage passes":
+                    int itemSellinBackstage = item.getSellIn();
+                    if (itemSellinBackstage <= 0)
+                        item.setQuality(0);
+                    else {
+                        updateBSAB(item);
+                    }
+                    break;
+
+                case "Aged Brie":
+                    updateBSAB(item);
+                    break;
+
+                case "Conjured":
+                    updateConjDefault(item,2);
+                    break;
+
+                case "Sulfuras":
+                    break;
+
+                default:
+                    updateConjDefault(item,1);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/pbz4esilv/gildedrose/InventoryTest.java
@@ -0,0 +1,391 @@
+package com.pbz4esilv.gildedrose;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by MERIEM on 02-Oct-15.
+ */
+
+
+
+
+public class InventoryTest {
+
+
+    /*
+     * Test si la qualité augmente ou diminue selon l'item après un update.
+     * et aucun changement pour l'objet "Sulfuras"
+     */
+    @Test
+    public void updateQuality_Test() throws Exception{
+
+        //Initialisation
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Conjured Mana Cake", 3, 6)
+        };
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(6,items.length);
+
+        Assert.assertEquals(20, items[0].getQuality());
+        Assert.assertEquals(0, items[1].getQuality());
+        Assert.assertEquals(7, items[2].getQuality());
+        Assert.assertEquals(80, items[3].getQuality());
+        Assert.assertEquals(20, items[4].getQuality());
+        Assert.assertEquals(6, items[5].getQuality());
+
+        Assert.assertEquals(10, items[0].getSellIn());
+        Assert.assertEquals(2, items[1].getSellIn());
+        Assert.assertEquals(5, items[2].getSellIn());
+        Assert.assertEquals(0, items[3].getSellIn());
+        Assert.assertEquals(15, items[4].getSellIn());
+        Assert.assertEquals(3, items[5].getSellIn());
+
+        //Compute
+        inventory.updateQuality();
+
+        //Assert
+        //Checking the quality and the sellin after updating.
+        Assert.assertEquals(6,items.length);
+
+        Assert.assertEquals(19,items[0].getQuality());
+        Assert.assertEquals(1,items[1].getQuality());
+        Assert.assertEquals(6, items[2].getQuality());
+        Assert.assertEquals(80,items[3].getQuality());
+        Assert.assertEquals(21,items[4].getQuality());
+        Assert.assertEquals(4, items[5].getQuality());
+
+        Assert.assertEquals(9, items[0].getSellIn());
+        Assert.assertEquals(1, items[1].getSellIn());
+        Assert.assertEquals(4, items[2].getSellIn());
+        Assert.assertEquals(0, items[3].getSellIn());
+        Assert.assertEquals(14, items[4].getSellIn());
+        Assert.assertEquals(2, items[5].getSellIn());
+
+
+    }
+
+
+    /*
+     * Test de l'objet legendaire "Sulfuras" => Aucun changement doit avoir lieu pour cet objet.
+     */
+    @Test
+    public void sulfuras_Test(){
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(80, items[0].getQuality());
+        Assert.assertEquals(0, items[0].getSellIn());
+
+        //update
+        inventory.updateQuality();
+
+        //Checking the quality and the sellin after updating.
+        Assert.assertEquals(80, items[0].getQuality());
+        Assert.assertEquals(0, items[0].getSellIn());
+
+    }
+
+
+    /*
+     * Test si les autres objets normaux ont leur SellIn et leur Quality qui baisse par 1
+     * avec le SellIn > 0.
+     */
+    @Test
+    public void lowerTheSellInByOneOthersItems_Test() throws Exception {
+        Item[] item = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Elixir of the Mongoose", 5, 7)
+        };
+
+        Inventory inventory = new Inventory(item);
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(10, item[0].getSellIn());
+        Assert.assertEquals(20, item[0].getQuality());
+        Assert.assertEquals(5, item[1].getSellIn());
+        Assert.assertEquals(7, item[1].getQuality());
+
+        //update
+        inventory.updateQuality();
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(9, item[0].getSellIn());
+        Assert.assertEquals(19, item[0].getQuality());
+        Assert.assertEquals(4, item[1].getSellIn());
+        Assert.assertEquals(6, item[1].getQuality());
+    }
+
+
+    /*
+     * Test que la qualité des items ne soient jamais négatives.
+     */
+    @Test
+    public void theQualityNeverNegative_Test() throws Exception {
+        //INIT
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 0),
+                new Item("Elixir of the Mongoose", 5, 0),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 0),
+                new Item("Conjured Mana Cake", 3, 0)
+        };
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality before updating.
+        Assert.assertEquals(4,items.length);
+
+        Assert.assertEquals(0, items[0].getQuality());
+        Assert.assertEquals(0, items[1].getQuality());
+        Assert.assertEquals(0, items[2].getQuality());
+        Assert.assertEquals(0, items[3].getQuality());
+
+
+        //Compute
+        inventory.updateQuality();
+
+        //Assert
+        //Test the quality before updating.
+        Assert.assertEquals(4,items.length);
+
+        Assert.assertEquals(0,items[0].getQuality());
+        Assert.assertEquals(0,items[1].getQuality());
+        Assert.assertEquals(0, items[2].getQuality());
+        Assert.assertEquals(0,items[3].getQuality());
+
+
+
+    }
+
+
+    /*
+     * Test que la qualité des items se dégradent deux fois plus vite une fois la date de vente passée.
+     */
+    @Test
+    public void theQualityDegratesTwiceAsFastOnceTheSellInDateHasPassed_Test() throws Exception {
+        //INIT
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", -2, 10),
+                new Item("Elixir of the Mongoose", -2, 20),
+                new Item("Conjured Mana Cake", -2, 6)
+
+        };
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality before updating.
+        Assert.assertEquals(10, items[0].getQuality());
+        Assert.assertEquals(20, items[1].getQuality());
+        Assert.assertEquals(6, items[2].getQuality());
+
+        //Compute
+        inventory.updateQuality();
+
+        //Assert
+        //Check the quality after updating.
+        Assert.assertEquals(8, items[0].getQuality());
+        Assert.assertEquals(18, items[1].getQuality());
+        Assert.assertEquals(2, items[2].getQuality());
+
+
+    }
+
+
+    /*
+     * Test sur l'item "Backstage passes to a TAFKAL80ETC concert".
+     */
+    @Test
+    public void backstage_Test() {
+
+        Item[] items= new Item[]
+                { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)};
+
+        Inventory inventory=new Inventory(items);
+
+		/*
+		 * increase backstage passes quality by 1 when the concert is more than 10 days away
+		 */
+        Assert.assertEquals(20, items[0].getQuality());
+        Assert.assertEquals(10, items[0].getSellIn());
+        inventory.updateQuality();
+        Assert.assertEquals(22, items[0].getQuality());
+        Assert.assertEquals(9, items[0].getSellIn());
+
+		/*
+		 *increase backstage passes quality by 2 when the concert is 10 days or less away
+		 *increase backstage passes quality by 3 when the concert is 5 days or less away
+		*/
+
+        //for 4 update the quality increase by 2 when there are 10 days at less and 3 for 5 day or less
+        //and 3 for 5 day or less
+        //22+4*2+3=33
+        for(int i=0;i<5;i++)
+        {
+            inventory.updateQuality();
+
+        }
+
+        Assert.assertEquals(33, items[0].getQuality());
+        Assert.assertEquals(4, items[0].getSellIn());
+
+        for(int i=0;i<5;i++)
+        {
+            inventory.updateQuality();
+        }
+
+		/*
+		 * lower backstage passes to Zero Quality once the concert has happened
+		 */
+        Assert.assertEquals(0, items[0].getQuality());
+        Assert.assertEquals(-1, items[0].getSellIn());
+    }
+
+
+    /*
+     * Test sur l'item "Backstage passes to a TAFKAL80ETC concert"
+     * si la date de vente est passé => la qualité de cet item vaudra 0.
+     */
+    @Test
+    public void lowerBackstagePassesToZeroQualityOnceConcertHasHappened_Test() throws Exception {
+        Item[] items= new Item[]
+                { new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)};
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(20, items[0].getQuality());
+        Assert.assertEquals(-1, items[0].getSellIn());
+
+        inventory.updateQuality();//update
+
+        //Checking the quality and the sellin after updating.
+        Assert.assertEquals(0, items[0].getQuality());
+        Assert.assertEquals(-2, items[0].getSellIn());
+    }
+
+
+    /*
+     *La qualité de "Backstage passes to a TAFKAL80ETC concert" ne doit pas dépasser 50.
+     */
+    @Test
+    public void BackstagePassesNotOverQualityof50() throws Exception {
+
+        Item[] items= new Item[]
+                {       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50),
+                        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48),
+                };
+
+        Inventory inventory=new Inventory(items);
+
+        //test the quality and the sellin before updating.
+        Assert.assertEquals(50, items[0].getQuality());
+        Assert.assertEquals(49, items[1].getQuality());
+        Assert.assertEquals(48, items[2].getQuality());
+
+        //update
+        inventory.updateQuality();
+
+        //check the quality and the sellin after updating.
+        Assert.assertEquals(50, items[0].getQuality());
+        Assert.assertEquals(50, items[1].getQuality());
+        Assert.assertEquals(50, items[2].getQuality());
+
+
+    }
+
+
+    /*
+     * Test sur l'item "Aged Brie".
+     * la qualité de cet item augmente.
+     */
+    @Test
+    public void agedBrie_Test() throws Exception {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 0)};
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(0, items[0].getQuality());
+        Assert.assertEquals(2, items[0].getSellIn());
+
+        inventory.updateQuality();//update
+
+        //Checking the quality and the sellin after updating.
+        Assert.assertEquals(1, items[0].getQuality());
+        Assert.assertEquals(1, items[0].getSellIn());
+    }
+
+
+    /*
+     * La qualité de "Aged Brie" ne peut pas dépasser 50
+     */
+    @Test
+    public void qualityAgedBrieNotOver50_Test() throws Exception {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 50)};
+
+        Inventory inventory=new Inventory(items);
+
+        //Test the quality and the sellin before updating.
+        Assert.assertEquals(50, items[0].getQuality());
+        Assert.assertEquals(2, items[0].getSellIn());
+
+        //update
+        inventory.updateQuality();
+
+        //Checking the quality and the sellin after updating.
+        Assert.assertEquals(50, items[0].getQuality());
+        Assert.assertEquals(1, items[0].getSellIn());
+    }
+
+
+    /*
+     * Test sur l'item "Conjured Mana Cake"
+     * "Conjured" items degrade in Quality twice as fast as normal items
+     */
+    @Test
+    public void conjured_Test() throws Exception {
+
+        Item[] items= new Item[]
+                { new Item("Conjured Mana Cake", 3, 6)};
+
+        Inventory inventory=new Inventory(items);
+
+        //test the quality and the sellin before updating.
+        Assert.assertEquals(6, items[0].getQuality());
+        Assert.assertEquals(3, items[0].getSellIn());
+
+        //update
+        inventory.updateQuality();
+
+        //Checking the quality and the sellin of Conjured Mana Cake
+        Assert.assertEquals(4,items[0].getQuality());
+        Assert.assertEquals(2, items[0].getSellIn());
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
