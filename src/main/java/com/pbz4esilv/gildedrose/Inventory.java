5c6a90eb2561341b770884666f538b7ff89871e0
MERIEM <mariam.chergui@devinci.fr>
2015-10-05 09:49:58 +0200

    Classe Item

--- src/main/java/com/pbz4esilv/gildedrose/Inventory.java
+++ src/main/java/com/pbz4esilv/gildedrose/Inventory.java
@@ -33,6 +33,7 @@ public class Inventory {
         this.items = items;
     }
 
+
     //Reusable method for updating Backstage and Aged Brie as nearly same constraints apply
     public void updateBSAB(Item item) {
         int itemSellin = item.getSellIn();
